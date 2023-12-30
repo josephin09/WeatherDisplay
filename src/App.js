2c1dfbd1a5aaede0e9614a9f047600aa6c1d5f72
josephin09 <josephindavidlatha@gmail.com>
2023-12-30 17:19:03 +0530

    re-run

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 const SearchBar = ({ onSearch }) => {
   const [city, setCity] = useState("");
-
   const handleSearch = () => {
     onSearch(city);
   };
 
+  const changeHandler = (e) => {
+    setCity(e.target.value);
+  };
   return (
     <div className="search-bar">
       <input
         type="text"
         value={city}
-        onChange={(e) => setCity(e.target.value)}
+        onChange={changeHandler}
         placeholder="Enter city name"
       />
       <button onClick={handleSearch}>Search</button>
@@ -34,41 +36,38 @@ const WeatherCard = ({ title, data }) => {
 const WeatherDisplay = ({ city }) => {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const getWeatherData = async () => {
+    try {
+      const res = await axios.get(
+        "https://api.weatherapi.com/v1/current.json",
+        {
+          params: {
+            key: "9ca3c8381979408db4f100735233012",
+            q: city,
+          },
+        }
+      );
+      setWeatherData(res.data);
+    } catch (err) {
+      console.error("Error fetching data", err);
+      alert("Failed to fetch weather data");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     if (city) {
       setLoading(true);
-      axios
-        .get("https://api.weatherapi.com/v1/current.json", {
-          params: {
-            key: "9ca3c8381979408db4f100735233012",
-            q: city
-          }
-        })
-        .then((response) => {
-          setWeatherData(response.data);
-          setError(null); // Clear any previous errors
-        })
-        .catch((error) => {
-          console.error("Error fetching data: ", error);
-          setError(error.message || "Failed to fetch weather data");
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+      getWeatherData();
     }
   }, [city]);
 
   return (
     <div className="weather-display">
       {loading && <p>Loading data...</p>}
-      {!loading && error && (
-        <div>
-          <p>Error: {error}</p>
-        </div>
-      )}
-      {!loading && !error && weatherData && (
+      {!loading && weatherData && (
         <div className="weather-cards">
           <WeatherCard
             title="Temperature"
@@ -80,7 +79,7 @@ const WeatherDisplay = ({ city }) => {
           />
           <WeatherCard
             title="Condition"
-            data={weatherData.current.condition.text}
+            data={`${weatherData.current.condition.text}`}
           />
           <WeatherCard
             title="Wind Speed"
@@ -92,11 +91,11 @@ const WeatherDisplay = ({ city }) => {
   );
 };
 
-function App() {
+export default function App() {
   const [city, setCity] = useState("");
 
-  const handleSearch = (searchCity) => {
-    setCity(searchCity);
+  const handleSearch = (searchedVal) => {
+    setCity(searchedVal);
   };
 
   return (
@@ -106,7 +105,5 @@ function App() {
     </div>
   );
 }
-
-export default App;
             
            
